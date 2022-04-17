938160b8135895cc9ca56e9e8bfee47c8cac2b6b
Georgia Burnside <burnsidegeorgia@gmail.com>
2022-04-17 19:05:10 -0400

    commit tree a24dc441496136d7588d8cf38e851ac0da7b8d12

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let userInput = event.target.searchByID.value;
 
-document.addEventListener('DOMContentLoaded', init);
+    fetch(`http://localhost:3000/movies/${userInput}`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        const movieTitle = document.querySelector("#movieDetails h4");
+        const movieSummary = document.querySelector("#movieDetails p");
+
+        movieTitle.innerText = data.title;
+        movieSummary.innerText = data.summary;
+        console.log(movieSummary);
+      });
+  });
+};
+document.addEventListener("DOMContentLoaded", init);
